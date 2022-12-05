72ec0bc8cd928f3da6c752017a3e0345bb3cf8d7
KGTH <kgthcruz94@gmail.com>
2022-12-05 11:12:17 +0000

    final

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable=False)
+    last_name= Column(String(80), nullable=False)
+    telephone= Column(Integer)
+    email= Column(String(80), nullable=False)
+    password = Column(String(80), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorito(Base):
+    __tablename__ = 'favorito'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_usuario = Column(Integer,ForeignKey('user.id'))
+    id_planets= Column(Integer,ForeignKey('planets.id'))
+    id_characters=Column(Integer,ForeignKey('characters.id'))
+    
+   # person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(80), nullable=False)
+    population= Column(Integer)
+    climate= Column(String(80), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(80), nullable=False)
+    gender= Column(String(80), nullable=False)
+    birth_years= Column(Integer)
 
     def to_dict(self):
         return {}
